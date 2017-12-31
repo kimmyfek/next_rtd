d5cef4a04ee0e25fdbc81153432f488d456273cc
Jason <jason_job@cable.comcast.com>
2017-12-31 00:02:49 -0700

    FUUUU

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ case "$OSTYPE" in
         ;;
 esac
 
-docker run --rm -v "$PWD":/go/src/github.com/kimmyfek/next_rtd -w /go/src/github.com/kimmyfek/next_rtd /bin/bash -c "golang:1.8 go get && export GOOS=$HOST_OS && go build -v -o nxt-darwin"
+docker run --rm -v "$PWD":/go/src/github.com/kimmyfek/next_rtd -w /go/src/github.com/kimmyfek/next_rtd golang:1.8 /bin/bash -c "go get && export GOOS=$HOST_OS && go build -v -o nxt-darwin"
